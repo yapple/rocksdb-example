1601508679c2ae342fe689a3888ffd2cab5ccfe2
wangyi <wangyi.ywq@bytedance.com>
2025-09-01 10:57:22 +0800

    udt demo

--- src/main/java/org/example/RocksDBConnector.java
+++ src/main/java/org/example/RocksDBConnector.java
@@ -20,6 +20,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.Random;
+import java.util.Arrays;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 /**
  * 
@@ -76,18 +79,58 @@ public class RocksDBConnector {
     }
     private static void db_load()throws RocksDBException{
 
-        for(int i =0 ;i< 1000;i++){
-            String key = getRandomString(8);
-            String value = getRandomString(i);
-            for(int j = 0;j < 1000;j++){ 
-                db.put(key.getBytes(),value.getBytes());
-            }
-            System.out.println("finished load" + String.valueOf(i));
+      for (int i = 0; i < 1000; i += 2) {
+        String key = getRandomString(8);
+        String value = getRandomString(i);
+        byte[] valueBytes = value.getBytes();
+        byte[] ts1 = encodeFixed64(i);
+        db.put(key.getBytes(), ts1, valueBytes);
+        byte[] ts2 = encodeFixed64(i + 1);
+        ReadOptions ro = new ReadOptions();
+        ro.setTimeStamp(new Slice(ts2));
+        byte[] vBytes = new byte[1024];
+        int r = db.get(ro, key.getBytes(), vBytes);
+        if (r > 0) {
+          byte[] vBytesSub = Arrays.copyOfRange(vBytes, 0, r);
+          if (Arrays.equals(vBytesSub, valueBytes)) {
+            System.out.println("v 和 value 相等");
+          } else {
+            System.out.println(vBytesSub);
+            System.out.println("v 和 value 不相等");
+          }
+        } else {
+            System.out.println("v 不存在");
         }
+        System.out.println("finished load" + String.valueOf(i));
+      }
         System.out.println("finished load");
         db.compactRange();
     }
-        
+
+    // 判断系统原生字节序是否为小端
+    private static final boolean IS_LITTLE_ENDIAN =
+        ByteOrder.nativeOrder() == ByteOrder.LITTLE_ENDIAN;
+
+    /**
+     * 将 long 类型编码为固定 8 字节的字节数组
+     * @param value 要编码的 long 值
+     * @return 编码后的 8 字节数组
+     */
+    public static byte[] encodeFixed64(long value) {
+      byte[] result = new byte[8];
+      ByteBuffer buffer = ByteBuffer.wrap(result);
+
+      // 根据系统原生字节序编码
+      if (IS_LITTLE_ENDIAN) {
+        buffer.order(ByteOrder.LITTLE_ENDIAN);
+      } else {
+        buffer.order(ByteOrder.BIG_ENDIAN);
+      }
+
+      buffer.putLong(value);
+      return result;
+    }
+
     private  static void initializeRocksDb() throws RocksDBException,UnsupportedEncodingException{
             RocksDB.loadLibrary();
             Options options = new Options().setCreateIfMissing(true);
@@ -96,14 +139,14 @@ public class RocksDBConnector {
             options.setWriteBufferSize(50L);
             options.setBlobSize(256);
             options.setCreateMissingColumnFamilies(true);
-            CompactionOptionsFIFO fifo = new CompactionOptionsFIFO();
-						options.setCompactionOptionsFIFO(fifo);
-						options.setCompactionStyle(CompactionStyle.FIFO);
-						options.setStatsDumpPeriodSec(10);
+            options.setComparator(BuiltinComparator.BYTEWISE_COMPARATOR_U64TS);
+            // CompactionOptionsFIFO fifo = new CompactionOptionsFIFO();
+            // options.setCompactionOptionsFIFO(fifo);
+            // options.setCompactionStyle(CompactionStyle.FIFO);
+            // options.setStatsDumpPeriodSec(10);
 
             if (db == null) {
                 db = RocksDB.open( options, "/tmp/testdata");
             }
     }
-
 }
